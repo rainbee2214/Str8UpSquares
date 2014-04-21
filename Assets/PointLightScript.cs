173814fdaf4c6ab834bea0dbc849149c218dd8d8
Sarah MacDonald <rainbee2214@gmail.com>
2014-04-21 17:32:22 -0300

    Updates

--- Assets/PointLightScript.cs
+++ Assets/PointLightScript.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PointLightScript : MonoBehaviour {
+public class PointLightScript : MonoBehaviour 
+{
+	public float size;
+	public int delay = 3;
+	
+	private bool done = false;
+	void Start()
+	{
+		gameObject.light.range = size;
+	}
+	void Update() 
+	{
+		if (Time.time > delay && !done)
+		{
+			size--;
+			if (size < 0) size = 0;
+			gameObject.light.range = size;
+		}
 
-	// Use this for initialization
-	void Start () {
-	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }

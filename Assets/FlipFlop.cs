b3a9604b035841019992415170a5c963c3a9a76f
rainbee2214 <rainbee2214@gmail.com>
2014-10-17 01:08:13 -0300

    Added a bit flipper :P for keeping track of the level and the score.

---
+++ Assets/FlipFlop.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipFlop : MonoBehaviour {
+	const int _ZERO = 0;
+	const int _ONE = 1;
+
+	GameObject zero, one;
+	SpriteRenderer spriteRenderer0;
+	SpriteRenderer spriteRenderer1;
+
+	int state = 0;
+	int whichToDisplay;
+	int currentlyDisplayed;
+
+	bool chooseZero = false;
+	bool chooseOne = false;
+	public bool flipNow = false;
+	
+	void Start () {
+		currentlyDisplayed = _ZERO;
+		whichToDisplay = _ZERO;
+		one = (transform.GetChild (1)).gameObject;
+		zero = (transform.GetChild (0)).gameObject;
+		spriteRenderer0 = one.GetComponent<SpriteRenderer>();
+		spriteRenderer1 = zero.GetComponent<SpriteRenderer>();
+		spriteRenderer1.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flipNow) 
+		{
+			flip ();
+		}
+		if (chooseZero)
+		{
+			state = 0;
+			spriteRenderer0.enabled = true;
+			spriteRenderer1.enabled = false;
+			chooseZero = false;
+		}
+		else if (chooseOne)
+		{
+			state = 1;
+			spriteRenderer0.enabled = false;
+			spriteRenderer1.enabled = true;
+			chooseOne = false;
+		}
+	}
+
+	public int getState()
+	{
+		return state;
+	}
+
+	public void flip()
+	{
+		flipNow = false;
+		if (state == 0)
+		{
+			chooseZero = false;
+			chooseOne = true;
+			state = 1;
+		}
+		else if (state == 1)
+		{
+			chooseZero = true;
+			chooseOne = false;
+			state = 0;
+		}
+	}
+}
---
+++ Assets/LevelTracker.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTracker : MonoBehaviour {
+
+	public GameObject flipFlop;
+	public int n = 16;
+	GameObject[] flipFlops;
+	GameObject f;
+	int state;
+	public bool flip;
+	// Use this for initialization
+	void Start () {
+		flipFlops = new GameObject[n];
+		for(int i = 0; i < n; i++)
+		{
+			flipFlops[i] = Instantiate (flipFlop) as GameObject;
+
+			flipFlops[i].name = "FlipFlop:" + i;
+			flipFlops[i].transform.parent = this.transform;
+			Vector3 position = new Vector3(19-i, 19, 1);
+			flipFlops[i].transform.position = position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int index = 0;
+		int afterFlip = 0;
+		if(flip)
+		{
+
+			flipABit ();
+		}
+	
+	}
+	// This function will always flip bits starting with the right most bit. If adds one.
+	void flipABit()
+	{
+		flip = false;
+		int index = 0;
+		int afterFlip = 0;
+		while (afterFlip != 1 && index < n)
+		{
+
+			flipFlops[index].gameObject.GetComponent<FlipFlop> ().flip ();
+			afterFlip = flipFlops[index].gameObject.GetComponent<FlipFlop>().getState();
+			index++;
+			//if the flip becomes a 0
+		}
+		//Debug.Log (getDecimal());
+		float decimalValue = getDecimal ();
+		Debug.Log (decimalValue);
+	}
+
+	float getDecimal()
+	{
+		float decimalValue = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (flipFlops[i].gameObject.GetComponent<FlipFlop>().getState() == 1)
+			{
+				decimalValue += Mathf.Pow(2, i);
+			}
+		}
+
+		return decimalValue;
+	}
+}
